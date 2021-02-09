eb52ddd6b9a262f5cd59629a4434983ecca650dd
Vivekanand P V <vivekanandpv@outlook.com>
2021-02-10 00:04:11 +0530

    done

--- src/middleware/DummyLogger.js
+++ src/middleware/DummyLogger.js
@@ -2,3 +2,8 @@ export const dummyLogger = (store) => (next) => (action) => {
   console.log('Executing dummy logger', action);
   next(action);
 };
+
+export const anotherLogger = (store) => (next) => (action) => {
+  console.log('Executing another logger', action);
+  next(action);
+};
--- src/store/Store.js
+++ src/store/Store.js
@@ -1,10 +1,11 @@
 import { applyMiddleware, createStore, compose } from 'redux';
-import { dummyLogger } from '../middleware/DummyLogger';
+import { anotherLogger, dummyLogger } from '../middleware/DummyLogger';
 import { appBaseReducer } from '../reducers';
 
 const enhancers = [
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(dummyLogger),
+  applyMiddleware(anotherLogger),
 ];
 
 export const appStore = createStore(
