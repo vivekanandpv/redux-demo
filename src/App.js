4f38d672c220e1e701947f68829ad58d9462909a
Vivekanand P V <vivekanandpv@outlook.com>
2021-02-09 23:33:53 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-function App() {
+import { useSelector } from 'react-redux';
+
+const App = () => {
+  const cities = useSelector((state) => state.cities);
+  const counter = useSelector((state) => state.counter);
   return (
     <div className='container p-5'>
-      <h3>Learning Redux</h3>
+      <h3>Learning Redux | Counter: {counter}</h3>
       <hr />
+      <ul>
+        {cities.map((c) => (
+          <li key={c}>{c}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './store/Store';
+import { Provider } from 'react-redux';
+import { appStore } from './store/Store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={appStore}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
