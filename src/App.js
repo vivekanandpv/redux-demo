fa32e860c9f1a362efcb9269a5a2b4e690630148
Vivekanand P V <vivekanandpv@outlook.com>
2021-02-09 23:53:04 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { addCityActionCreator } from './actions/city';
+import { incrementActionCreator } from './actions/counter';
 
 const App = () => {
   const cities = useSelector((state) => state.cities);
   const counter = useSelector((state) => state.counter);
+
+  const dispatch = useDispatch();
+
+  const list = ['Mumbai', 'Bengaluru', 'Chennai', 'Kanpur'];
+
+  const addAllCities = () => {
+    list.forEach((c) => {
+      dispatch(addCityActionCreator(c));
+    });
+  };
+
   return (
     <div className='container p-5'>
       <h3>Learning Redux | Counter: {counter}</h3>
@@ -12,6 +25,16 @@ const App = () => {
           <li key={c}>{c}</li>
         ))}
       </ul>
+
+      <button className='btn btn-primary' onClick={addAllCities}>
+        Add All Cities
+      </button>
+      <button
+        className='btn btn-warning ml-3'
+        onClick={() => dispatch(incrementActionCreator())}
+      >
+        Increment
+      </button>
     </div>
   );
 };
