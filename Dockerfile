d3d87e2ad4fa0f50c7e799d1bd7518473dde02c3
Vivekanand P V <vivekanandpv@outlook.com>
2022-03-11 12:59:23 +0530

    done

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM nginx
+COPY build /usr/share/nginx/html
+
+
+#   to build: npm run dockerize
+#   to run: docker container run --publish 9001:80 --name redux-demo-container --detach redux-demo:1.0.1
+#   to check: http://localhost:9001
--- src/store/Store.js
+++ src/store/Store.js
@@ -2,12 +2,11 @@ import { applyMiddleware, createStore, compose } from 'redux';
 import { appBaseReducer } from '../reducers';
 import reduxLogger from 'redux-logger';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 const storeMiddleware = [applyMiddleware(reduxLogger), applyMiddleware(thunk)];
 
 export const appStore = createStore(
   appBaseReducer,
   { cities: [], counter: 0 },
-  composeWithDevTools(...storeMiddleware)
+  compose(...storeMiddleware)
 );
