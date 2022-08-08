a3c3771fbd8998008aab71e604f3d5d36df5dc33
Vivekanand P V <vivekanandpv@outlook.com>
2022-08-08 16:17:06 +0530

    added function expansion

--- src/middleware/DummyLogger.js
+++ src/middleware/DummyLogger.js
@@ -7,3 +7,12 @@ export const anotherLogger = (store) => (next) => (action) => {
   console.log('Executing another logger', action);
   next(action);
 };
+
+// export const dummyLogger1 = function (store) {
+//   return function (next) {
+//     return function (action) {
+//       console.log('Executing dummy logger', action);
+//       next(action);
+//     };
+//   };
+// };
